Fix dimension mismatch in accuracy calculation caused by hardcoded +5 classes

methods/base.py
@@ -284,7 +284,7 @@ class BaseLearner(object):
 
     def _evaluate(self, y_pred, y_true):
         ret = {}
-        grouped, new_acc = accuracy(y_pred.T[0], y_true, self._known_classes, increment=1)
+        grouped, new_acc = accuracy(y_pred.T[0], y_true, self._known_classes, self._total_classes, increment=1)
         ret["grouped"] = grouped
         ret["top1"] = grouped["total"]
         ret["top{}".format(self.topk)] = np.around(

utils/toolkit.py
@@ -30,7 +30,7 @@ def makedirs(path):
         os.makedirs(path)
 
 
-def accuracy(y_pred, y_true, nb_old, increment=10):
+def accuracy(y_pred, y_true, nb_old, nb_total, increment=10):
     assert len(y_pred) == len(y_true), "Data length error."
     all_acc = {}
     new_acc = []
@@ -39,7 +39,7 @@ def accuracy(y_pred, y_true, nb_old, increment=10):
     )
 
     # Grouped accuracy
-    class_boundaries = range(0, nb_old + 5, increment)
+    class_boundaries = range(0, nb_total, increment)
     for class_id in class_boundaries:
         class_label = "{}".format(str(class_id).rjust(2, "0"))
         class_indices = np.where(np.logical_and(y_true >= class_id, y_true < class_id + increment))[0]